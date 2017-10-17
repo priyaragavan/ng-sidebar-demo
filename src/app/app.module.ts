9bd5a3a6177e50d93e212ce482ac1db90ac8a941
priyaragavan <struggleguy@gmail.com>
2017-10-17 17:33:30 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// import { RouterModule } from '@angular/router';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+
+import { MatButtonModule, MatInputModule } from '@angular/material';
+
+import { SidebarModule } from 'ng-sidebar';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SidebarModule.forRoot(),
+    FormsModule,
+    // RouterModule.forRoot(),
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
